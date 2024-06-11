4e32a271a892d88318d7b5e4f4a58687e1e178c6
Nemesh Dima <dimitrijnemesh@gmail.com>
2024-06-11 19:50:43 +0300

    Another option for extraReducers

--- frontend/src/components/BookForm/BookForm.js
+++ frontend/src/components/BookForm/BookForm.js
@@ -35,7 +35,7 @@ const BookForm = () => {
   }
 
   const handleAddRandomBookViaAPI = () => {
-    dispatch(fetchBook())
+    dispatch(fetchBook("http://localhost:4000/random-book"))
   }
 
   return (
--- frontend/src/redux/slices/booksSlice.js
+++ frontend/src/redux/slices/booksSlice.js
@@ -2,13 +2,22 @@ import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 import axios from "axios"
 
 import createBookWithID from "../../utils/createBookWithId"
+import { setError } from "./errorSlice"
 
 const initialState = []
 
-export const fetchBook = createAsyncThunk("books/fetchBook", async () => {
-  const res = await axios.get("http://localhost:4000/random-book")
-  return res.data
-})
+export const fetchBook = createAsyncThunk(
+  "books/fetchBook",
+  async (url, thunkAPI) => {
+    try {
+      const res = await axios.get(url)
+      return res.data
+    } catch (error) {
+      thunkAPI.dispatch(setError(error.message))
+      throw error
+    }
+  }
+)
 
 const booksSlice = createSlice({
   name: "books",
@@ -37,6 +46,17 @@ const booksSlice = createSlice({
       //   )
     },
   },
+  // 1 OPTION
+  // extraReducers: {
+  //   [fetchBook.fulfilled]: (state, action) =>{
+  //     if (action.payload.title && action.payload.author) {
+  //             state.push(createBookWithID(action.payload, "API"))
+  //           }
+  //   }
+  // }
+
+  // 2 OPTION
+
   extraReducers: (builder) => {
     builder.addCase(fetchBook.fulfilled, (state, action) => {
       if (action.payload.title && action.payload.author) {
